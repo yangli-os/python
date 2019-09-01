492480d167c157e976246291674cce1342264c7c
16647615268 <47737324+16647615268@users.noreply.github.com>
2019-09-01 23:04:33 +0800

    Update read_time.py

--- 自己编写的小程序/筛选特征批量作图/read_time.py
+++ 自己编写的小程序/筛选特征批量作图/read_time.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import csv
 data = pd.read_csv(r"allFMData(1)(1)+-+副本.csv",encoding="gbk")
-col = data['Time'].str.isdigit()
+col = data['Time'].str.isdigit()            #如果字符串只包含数字则返回 True 否则返回 False
 cleaned = data[col].copy()
 print(cleaned)
 cleaned.to_csv("allFMData.csv")
