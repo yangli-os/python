1dc149e94c6955924a4dea445ffeaeb65399f4e1
YangLi <47737324+16647615268@users.noreply.github.com>
2020-07-29 17:26:50 +0800

    Update reload_song.py
    #编辑wav格式的音频文件，将音频文件添加上头，该头可以直接在web端播放

--- 自己编写的小程序/reload_song.py
+++ 自己编写的小程序/reload_song.py
@@ -1,3 +1,4 @@
+#编辑wav格式的音频文件，将音频文件添加上头，该头可以直接在web端播放
 import struct
 from pydub.audio_segment import AudioSegment
 import wave
@@ -10,6 +11,7 @@ def writeframesraw(b):
     headers += b'RIFF'
     nframes = int(b.frame_count())
     datalength = nframes * b.channels * b.sample_width
+    #添加头
     headers += struct.pack('<L4s4sLHHLLHH4s',
                            36 + datalength, b'WAVE', b'fmt ', 16,
                            WAVE_FORMAT_PCM, b.channels, b.frame_rate,
