8435b52bbd9d4f0c01b23ca63a08c790a1da999e
YangLi <47737324+16647615268@users.noreply.github.com>
2020-07-30 20:05:47 +0800

    Add files via upload
    # reload_song.py
    #编辑wav格式的音频文件，将音频文件添加上头，该头可以直接在web端播放
    
    # reload_syn_syn_en.py
    通过requests方式获取post服务中的文件并下载

---
+++ Voice broadcast number/reload_song.py
@@ -0,0 +1,24 @@
+import struct
+from pydub.audio_segment import AudioSegment
+import wave
+with open("oneyuan.wav","rb") as f1:
+    data = f1.read()
+old_data = AudioSegment.from_wav("oneyuan.wav")
+def writeframesraw(b):
+    WAVE_FORMAT_PCM = 0x0001
+    headers = bytes()
+    headers += b'RIFF'
+    nframes = int(b.frame_count())
+    datalength = nframes * b.channels * b.sample_width
+    headers += struct.pack('<L4s4sLHHLLHH4s',
+                           36 + datalength, b'WAVE', b'fmt ', 16,
+                           WAVE_FORMAT_PCM, b.channels, b.frame_rate,
+                           b.channels * b.frame_rate * b.sample_width,
+                           b.channels * b.sample_width,
+                           b.sample_width * 8, b'data')
+    headers += struct.pack('<L', datalength)
+    ret = headers + b.raw_data
+    return ret
+new_data = writeframesraw(old_data)
+with open("int.wav","wb") as f1:
+    f1.write(new_data)
