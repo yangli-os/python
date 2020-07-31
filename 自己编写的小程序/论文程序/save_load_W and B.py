4da97f25a432bd4ece865e0f760641cb7ef14e2e
YangLi <47737324+16647615268@users.noreply.github.com>
2020-07-31 15:40:22 +0800

    Delete save_load_W and B.py

--- 自己编写的小程序/论文程序/save_load_W and B.py
+++
@@ -1,9 +0,0 @@
-try:
-    model.load_weights("SaveModel/cifarCnnModel.h5")      #模型存储在SaveModel文件夹下
-    print('模型加载成功，继续训练模型')
-except:
-    print("加载模型失败，开始训练新模型")
-
-
-model.save_weights("SaveModel/cifarCnnModel.h5")       #模型存储在SaveModel文件夹下
-print("模型保存成功")
