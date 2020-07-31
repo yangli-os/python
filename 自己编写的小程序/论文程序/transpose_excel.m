ea1980d23da59cc8e76fbd2d02fc962b122d3139
YangLi <47737324+16647615268@users.noreply.github.com>
2020-07-31 15:41:30 +0800

    Delete transpose_excel.m

--- 自己编写的小程序/论文程序/transpose_excel.m
+++
@@ -1,5 +0,0 @@
-%% 将Excel表的矩阵转置
-outputfile = xlsread('C:\Users\liyang\Desktop\bianli2/data.xls');      %第一步读取要写入的excel文件
-rangefile = [outputfile'];
-xlswrite('C:\Users\liyang\Desktop\bianli2/data2.xls',rangefile);       %转置后的新表
-disp('处理完成');
