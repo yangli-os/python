33a3b1a76aaae5c63e3976a61fcfd71094ad22c1
YangLi <47737324+16647615268@users.noreply.github.com>
2021-05-12 17:35:35 +0800

    Update read_ch.py

--- 自己编写的小程序/read_ch.py
+++ 自己编写的小程序/read_ch.py
@@ -3,14 +3,14 @@ import pymysql
 db = pymysql.Connect(host = 'localhost',
                        port = 3306,
                        user = 'root',
-                       passwd = 'Nc1952',
-                       db = 'heartdiag',
+                       passwd = '123456',
+                       db = 'database',
                        charset='utf8')
 
 cursor = db.cursor() #创建一个游标
 # 创建游标对象
 print("connect success")
-cursor.execute("select * from all_chs")
+cursor.execute("select * from chs")
 des = cursor.description
 print("表的描述:", des)
 # data = cursor.fetchall()  # 全量查询结果
