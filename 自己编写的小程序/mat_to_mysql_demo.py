90353a802e5a8290229f2067312aba77413bb116
YangLi <47737324+16647615268@users.noreply.github.com>
2021-05-12 17:34:53 +0800

    Update mat_to_mysql_demo.py

--- 自己编写的小程序/mat_to_mysql_demo.py
+++ 自己编写的小程序/mat_to_mysql_demo.py
@@ -6,18 +6,18 @@ import pymysql
 db = pymysql.Connect(host = 'localhost',
                        port = 3306,
                        user = 'root',
-                       passwd = 'Nc1952',
-                       db = 'heartdiag',)
+                       passwd = '123456',
+                       db = 'database',)
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
-dataFile = r'.\QZFM_0.mat'            #同级目录下的相对路径
+dataFile = r'.\data.mat'            #同级目录下的相对路径
 data = scio.loadmat(dataFile)
 print(data.keys())
 data_set = data["Bz"]
 
 insert_number = 1000                #设置单次写入数据库的数据量
 for col_num in range(0, len(data_set), insert_number):
-    # 获得单行的32个数据，转换成元组列表
+    # 获得单行的16个数据，转换成元组列表
     insert_val = []
     for col_simple in range(insert_number):
         val_simple = (tuple(data_set[col_num:col_num+insert_number][col_simple]))
