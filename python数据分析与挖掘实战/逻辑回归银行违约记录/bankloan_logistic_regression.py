0ccd3411577f8ce25bd907dfd1b4019e7b755618
YangLi <47737324+yangli-os@users.noreply.github.com>
2021-10-26 13:41:22 +0800

    Update bankloan_logistic_regression.py

--- python数据分析与挖掘实战/逻辑回归银行违约记录/bankloan_logistic_regression.py
+++ python数据分析与挖掘实战/逻辑回归银行违约记录/bankloan_logistic_regression.py
@@ -4,8 +4,8 @@ import pandas as pd
 #参数初始化
 filename = '../data/bankloan.xls'
 data = pd.read_excel(filename)
-x = data.iloc[:,:8].as_matrix()
-y = data.iloc[:,8].as_matrix()
+x = data.iloc[:,:8].value
+y = data.iloc[:,8].value
 
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.linear_model import RandomizedLogisticRegression as RLR 
@@ -14,7 +14,7 @@ rlr.fit(x, y) #训练模型
 rlr.get_support() #获取特征筛选结果，也可以通过.scores_方法获取各个特征的分数
 print ('通过随机逻辑回归模型筛选特征结束')
 print (u'有效特征为：%s' % ','.join(data.loc[:,rlr.get_support()]))
-x = data.loc[:,rlr.get_support()].as_matrix() #筛选好特征
+x = data.loc[:,rlr.get_support()].value #筛选好特征
 
 lr = LR() #建立逻辑货柜模型
 lr.fit(x, y) #用筛选后的特征数据来训练模型
